13af54f1d772ca87f29586c6467829bd2bf4302f
Sati Kultueva <satironius@icloud.com>
2025-09-30 14:35:34 +0300

    Added comments

--- circle.py
+++ circle.py
@@ -1,10 +1,11 @@
 import math
 
-
 def area(r):
+    '''Принимает на вход радиус (r) возвращает площадь круга'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает на вход радиус (r), возвращает периметр круга'''
     return 2 * math.pi * r
 
